3f43831bf28e926bd03f5dcf28aa884906bd4325
mxbranch <dan@mxbranch.com>
2022-05-19 11:58:47 +0100

    Basic functionality
    This commit adds central rot13 functionality.
    
    It also adds a flag -caps which forces output to be capitalised.

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 A simple command line utility which performs rot13 encryption. Project intended for learning the go toolkit/standards.
 
+## WIP
+
+This utility currently:
+
+* Can perform rot13 encoding on ascii text while passing through whitespace and punctuation.
+* Has an optional flag -caps to output the ciphertext in caps (as is traditional!)
+
+In the future the following features will be added:
+
+* Reading/writing files
+
 ## Description
 
-*rot13* is a standard shift cipher which shifts letters 13 places through the alphabet - ensuring that the same function can be used to encode and decode text.
+*rot13* is a standard shift cipher which shifts letters 13 places through the alphabet - ensuring that the same function can be used to encode and decode text. *rot13* is most often used as a technique for disguising spoilers in communication methods that don't have a built-in method for hiding text. I made this utility because it was a reasonable way to learn some of the go toolkit and also because having a command line utility for this seemed fun!
--- rot13.go
+++ rot13.go
@@ -1,7 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	s "strings"
+)
 
 func main() {
-	fmt.Println("Great!")
+
+	// Set up command line flags
+	// -caps is a boolean flag to toggle ciphertext being presented in all caps
+	// default: false
+	var capToggle bool
+	flag.BoolVar(&capToggle, "caps", false, "Should ciphertext be presented in all caps? Default: false")
+
+	// -read gets a path to a text file which the utility loads to encrypt.
+	// default: ""
+	var readPath string
+	flag.StringVar(&readPath, "read", "", "NOT IMPLEMENTED: Path to a text file to read. Default: empty")
+
+	// -write gets a path to a text file which the utility will output the encrypted message to.
+	// default: ""
+	var writePath string
+	flag.StringVar(&writePath, "write", "", "NOT IMPLEMENTED: Path to a text file to write the result to. Default: empty")
+
+	flag.Parse()
+
+	// get plaintext by converting arguments from array to single string
+
+	var plaintext string = ""
+	for _, arg := range flag.Args() {
+		plaintext += arg
+		plaintext += " "
+	}
+
+	// go through plaintext one rune at a time and parse each character
+
+	var ciphertext string = ""
+	for _, rn := range plaintext {
+		ciphertext += runeShifter(rn)
+	}
+
+	// capitalise the ciphertext if requested
+
+	if capToggle {
+		ciphertext = s.ToUpper(ciphertext)
+	}
+
+	fmt.Println(ciphertext)
+
+}
+
+func runeShifter(rn rune) string {
+	// we need to treat capital and lower case letters differently
+	// so first we need to determine which is happening here
+
+	if rn >= 65 && rn <= 90 {
+
+		// capitals - 65 - 90
+
+		rn += 13
+		if rn > 90 {
+			rn -= 26
+		}
+
+	} else if rn >= 97 && rn <= 122 {
+
+		// lower case - 97 - 122
+
+		rn += 13
+		if rn > 122 {
+			rn -= 26
+		}
+
+	}
+
+	// other (punc, whitespace, etc)
+	// these pass through without change
+
+	return string(rn)
+
 }
